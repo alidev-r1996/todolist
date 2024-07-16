c45082ffa27db30d21aa6782fa473cb4027fc124
Ali.r <alirahimi611@gmail.com>
2024-07-16 18:00:01 +0430

    bg bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import FilterTodo from "@/components/FilterTodo";
 import TodoList from "@/components/TodoList";
 export default async function Home() {
   return (
-    <main className="bg-gray-100 w-screen h-screen">
+    <section >
       <div className="flex lg:flex-row flex-col-reverse items-start gap-4 p-4 ">
         <AddTodo />
-        <div className=" sm:grid sm:grid-rows-[36px,_max] sm:grid-cols-5 flex flex-col gap-2 gap-y-4 w-full">
+        <div className=" sm:grid sm:grid-rows-[36px,_auto] sm:grid-cols-5 flex flex-col gap-2 gap-y-4 w-full">
           <FilterTodo />
           <TodoList />
         </div>
       </div>
-    </main>
+    </section>
   );
 }
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -10,12 +10,12 @@ export const Todo: React.FC<TodoProps> = ({created_at,desc, _id,isCompleted,titl
       className={`flex relative cursor-pointer items-start sm:items-center justify-between p-2 bg-gray-100 rounded`}
     >
       <div className="flex flex-col flex-1">
-        <h1 className={`${isCompleted && "line-through text-rose-700"} font-bold text-sm`}>
+        <h1 className={`${isCompleted && "line-through text-rose-700"} flex-1 font-bold text-sm`}>
           {title}
         </h1>
         <p className="text-[10px]  text-gray-400">{DateString(created_at)}</p>  
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center ">
         <TodoActions todo={{title, desc, _id, isCompleted}} />
       </div>
     </div>
